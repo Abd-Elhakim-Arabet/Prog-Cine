471a99da2b7a4ce563da748c8a2baa9016fa9872
Abd-Elhakim-Arabet <abd.elhakim.arabet@ensia.edu.dz>
2024-12-17 16:15:23 +0100

    return_colors

--- lib/assets/colors.dart
+++ lib/assets/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-static const Color myBackground = Color.fromRGBO(200, 159, 216, 1);
-  static const Color myPrimary = Color.fromRGBO(215, 3, 116, 1);
-  static const Color myYellow = Color.fromRGBO(255, 255, 255, 1);
-  static const Color myAccent = Color.fromRGBO(221, 120, 174, 1);
+static const Color myBackground = Color.fromRGBO(71, 18, 110, 1);
+  static const Color myPrimary = Color.fromRGBO(226, 0, 122, 1);
+  static const Color myYellow = Color.fromRGBO(255, 242, 0, 1);
+  static const Color myAccent = Color(0xFFFFFFFF);
 }
