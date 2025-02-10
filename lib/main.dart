a66ef7f2cbe693625ddef8b84cdeed2ad89df03d
MHND09 <131745373+MHND09@users.noreply.github.com>
2025-02-10 22:29:22 +0100

    another one bites the dust

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prog/api/firebase_api.dart';
 import 'package:prog/app.dart';
 import 'package:firebase_core/firebase_core.dart';
